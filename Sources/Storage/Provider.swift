177c480872b9f0331c9382e4ced1a2495f99bab0
Brett Toomey <brettcwx@gmail.com>
2016-12-19 16:54:08 -0800

    More tests, public interface.

--- Sources/Storage/Provider.swift
+++ Sources/Storage/Provider.swift
@@ -1,15 +1,73 @@
+import S3
 import Vapor
+import S3SignerAWS
 
 public final class Provider: Vapor.Provider {
-    public var provided: Providable
+    public enum Error: Swift.Error {
+        case missingConfigurationFile
+        case unsupportedDriver(String)
+        case missingAccessKey
+        case missingSecretKey
+        case unknownRegion(String)
+    }
+    
+    public var provided: Providable {
+        return Providable()
+    }
     
     public init(config: Config) throws {
-        provided = Providable()
+        guard let config = config["storage"] else {
+            throw Error.missingConfigurationFile
+        }
+        
+        let networkDriver = try buildNetworkDriver(config: config)
+        Storage.networkDriver = networkDriver
     }
     
-    public func afterInit(_ drop: Droplet) {
+    public func boot(_ drop: Droplet) {}
+    
+    public func afterInit(_ drop: Droplet) {}
+    
+    public func beforeRun(_: Droplet) {}
+    
+    private func buildNetworkDriver(config: Config) throws -> NetworkDriver {
+        let template = config["template"]?.string ?? "$folder/$file"
+        let networkDriver: NetworkDriver
+        let driver = config["driver"]?.string ?? "aws"
+        switch driver {
+        case "aws":
+            networkDriver = try buildS3Driver(config: config, template: template)
+        default:
+            throw Error.unsupportedDriver(driver)
+        }
+        
+        return networkDriver
     }
     
-    public func beforeRun(_: Droplet) {
+    private func buildS3Driver(config: Config, template: String) throws -> S3Driver {
+        guard let accessKey = config["accessKey"]?.string else {
+            throw Error.missingAccessKey
+        }
+        
+        guard let secretKey = config["secretKey"]?.string else {
+            throw Error.missingSecretKey
+        }
+        
+        let bucket = config["bucket"]?.string
+        
+        let regionString = config["region"]?.string ?? "eu-west-1"
+        guard let region = Region(rawValue: regionString) else {
+            throw Error.unknownRegion(regionString)
+        }
+        
+        let s3 = S3(
+            accessKey: accessKey,
+            secretKey: secretKey,
+            bucketName: bucket,
+            region: region
+        )
+        
+        let pathBuilder = try ConfigurablePathBuilder(template: template)
+        return S3Driver(s3: s3, pathBuilder: pathBuilder)
     }
 }
--- Tests/StorageTests/StorageTests.swift
+++ Tests/StorageTests/StorageTests.swift
@@ -2,4 +2,11 @@ import XCTest
 @testable import Storage
 
 class StorageTests: XCTestCase {
+    static var allTests = [
+        ("testExample", testExample)
+    ]
+    
+    func testExample() {
+        XCTAssertEqual(2+2, 4)
+    }
 }
