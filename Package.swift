1d03d339cc484c24dcade2be409912fbe8bb8b8d
Masayuki Ono <mono0926@gmail.com>
2017-09-01 16:14:45 +0900

    Change aws dependency temporarily

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/nodes-vapor/data-uri.git", majorVersion: 1),
-        .Package(url: "https://github.com/nodes-vapor/aws.git", majorVersion: 1),
+        .Package(url: "https://github.com/mono0926/aws.git", majorVersion: 1),
         .Package(url: "https://github.com/manGoweb/MimeLib.git", majorVersion: 1)
     ]
 )
