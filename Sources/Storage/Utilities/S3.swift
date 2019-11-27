0540325c1b3463e87e413e95a7b01fd30f6b3723
Mattias Bowallius <mattias@ustwo.com>
2019-11-27 08:48:46 +0100

    Implemented get for S3

--- Sources/Storage/Utilities/S3.swift
+++ Sources/Storage/Utilities/S3.swift
@@ -352,6 +352,36 @@ public struct S3: Service {
         req.http.url = url
         return client.send(req)
     }
+    
+    public func get(
+        path: String,
+        contentType: String = "application/x-www-form-urlencoded; charset=utf-8",
+        access: AccessControlList = .publicRead,
+        on container: Container
+    ) throws -> Future<Response> {
+        guard let url = URL(string: generateURL(for: path)) else {
+            throw Error.invalidPath
+        }
+
+        let signedHeaders = try signer.sign(
+            contentType: contentType,
+            method: .get,
+            path: path,
+            headers: ["x-amz-acl": access.rawValue]
+        )
+
+        var headers: HTTPHeaders = [:]
+        signedHeaders.forEach {
+            headers.add(name: $0.key, value: $0.value)
+        }
+
+        let client = try container.client()
+        let req = Request(using: container)
+        req.http.method = .GET
+        req.http.headers = headers
+        req.http.url = url
+        return client.send(req)
+    }
 }
 
 extension S3 {
